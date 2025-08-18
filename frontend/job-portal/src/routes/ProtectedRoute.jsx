7d3203a62efc83a3a6f9198f521e785efb14461b
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-08-18 12:14:34 -0300

    criar esquema de rotas

---
+++ frontend/job-portal/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate, Outlet, useLocation} from 'react-router-dom'
+
+const ProtectedRoute = ({requireRole}) -> {
+    // TODO: será implementada depois
+    return <Outlet/>
+}
+
+export default ProtectedRoute;
