019118637d8b9bc9e8fcfce847f3af2b3609eac6
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-08-21 10:02:36 -0300

    configurar a arquitetura da aplicação backend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+/* Middleware to handle CORS */
+appendFile.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+/* Connect Database */
+connectDB();
+
+/* Middleware */
+appendFile.use(express.json());
+
+/* Routes */
+/* app.use("/api/auth", authRoutes)  */
+
+/* Server uploads folder */
+appendFile.use("uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+/* Start Server */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
